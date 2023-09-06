93c89ef0dea871ff43b8dae807b621ebe8f83e91
root <pyybor@gmail.com>
2023-09-06 07:34:00 +0000

    new large update, bug fixes, code is optimized

--- src/deeptlf.py
+++ src/deeptlf.py
@@ -1,304 +1,245 @@
+import re
 import numpy as np
-from tqdm import tqdm 
-import xgboost as xgb
-from sklearn.model_selection import train_test_split
-from sklearn.base import BaseEstimator
-from sklearn.metrics import roc_auc_score, r2_score
 
-
-# load PyTorch
 import torch
 import torch.nn as nn
-from torch.utils.data import Dataset
-from torch.utils.data import Dataset, DataLoader
-import torch.nn.functional as F
-from adabelief_pytorch import AdaBelief
+from torch.utils.data import DataLoader, Dataset
+
+from sklearn.base import BaseEstimator
+from sklearn.model_selection import train_test_split
+
+import xgboost as xgb
+from tqdm import tqdm
+
 from .tde import TreeDrivenEncoder
 
 
 class DeepTFL(BaseEstimator):
-    def __init__(
-        self, n_est: int = 23, max_depth: int = 4, drop: float = 0.23, xgb_lr: float = 0.5, batchsize: int = 320,
-        nn: int = 128, n_layers: int = 4, task: str = 'class', debug: bool = False, checkpoint_name: str = 'checkpoint.pt'
-    ):
-        """
-        A Deep Tree-Boosted Neural Network classifier/regressor.
-        """
-        self.xgb_model = None
-        self.task = task
-        self.nn_model = None
-        self.TDE_encoder = None
-        self.shape = None
+    """
+    A deep learning model based on XGBoost and a custom neural network.
 
+    Parameters
+    ----------
+    n_est : int, optional
+        Number of estimators for XGBoost model, default is 23.
+    max_depth : int, optional
+        Max depth for each tree in XGBoost, default is 4.
+    n_epoch : int, optional
+        Number of epochs for neural network training, default is 100.
+    hidden_dim : int, optional
+        Hidden layer dimensions for neural network, default is 128.
+    drop : float, optional
+        Dropout rate for neural network, default is 0.23.
+    xgb_lr : float, optional
+        Learning rate for XGBoost model, default is 0.5.
+    n_layers : int, optional
+        Number of layers in the neural network, default is 4.
+    checkpoint_name : str, optional
+        File name to save the neural network model, default is 'checkpoint.pt'.
+    batch_size : int, optional
+        Batch size for neural network training, default is 320.
+    task : str, optional
+        Type of machine learning task ('class' for classification, other values for regression), default is 'class'.
+    debug : bool, optional
+        Whether to print debugging information, default is False.
+
+    Attributes
+    ----------
+    xgb_model : XGBClassifier or XGBRegressor
+        Fitted XGBoost model.
+    nn_model : NeuralNet
+        Fitted neural network model.
+    TDE_encoder : TreeDrivenEncoder
+        Fitted Tree-Driven Encoder.
+    input_shape : int
+        Shape of the input feature space.
+    device : torch.device
+        Device used for computations ('cuda' or 'cpu').
+    """
+
+    def __init__(
+        self,
+        n_est=23,
+        max_depth=4,
+        drop=0.23,
+        xgb_lr=0.5,
+        batch_size=320,
+        n_epoch=100,
+        hidden_dim=256,
+        n_layers=4,
+        task="class",
+        debug=False,
+        checkpoint_name="checkpoint.pt",
+    ):
         self.n_est = n_est
         self.max_depth = max_depth
-        self.nn = nn
+        self.n_epoch = n_epoch
+        self.hidden_dim = hidden_dim
         self.drop = drop
         self.debug = debug
         self.xgb_lr = xgb_lr
         self.n_layers = n_layers
         self.checkpoint_name = checkpoint_name
-        self.batchsize = batchsize
-
-    def fit(self, X_train: np.ndarray, y_train: np.ndarray, X_val: np.ndarray = None, y_val: np.ndarray = None):
-        train_data = xgb.DMatrix(X_train, y_train)
-        if self.task == 'class':
-            gbdt_model = xgb.XGBClassifier
-            num_of_outputs = len(set(y_train))
-        else:
-            gbdt_model = xgb.XGBRegressor
-            num_of_outputs = 1
-
-        self.xgb_model = gbdt_model(learning_rate=self.xgb_lr, n_jobs=-1,
-                                     max_depth=self.max_depth,
-                                     n_estimators=self.n_est,
-                                     use_label_encoder=False,
-                                     )
-        self.xgb_model.fit(X_train, y_train)
-
-        trees = self.xgb_model.get_booster().get_dump(with_stats=False)
-
+        self.batch_size = batch_size
+        self.task = task
+        self.xgb_model = None
+        self.nn_model = None
         self.TDE_encoder = TreeDrivenEncoder()
+        self.input_shape = None
+        self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+    def fit(self, X_train, y_train, X_val=None, y_val=None):
+        self.fit_xgb(X_train)
+        trees = self.xgb_model.get_booster().get_dump(with_stats=False)
         self.TDE_encoder.fit(trees)
-
         enc_X_train = self.TDE_encoder.transform(X_train)
-        self.shape = enc_X_train.shape[1]
+        self.input_shape = enc_X_train.shape[1]
+        self.fit_nn(enc_X_train, y_train, X_val, y_val)
 
+    def fit_xgb(self, X_train):
+        # Using XGBRegressor for self-supervised learning.
+        self.xgb_model = xgb.XGBRegressor(
+            learning_rate=self.xgb_lr,
+            n_jobs=-1,
+            max_depth=self.max_depth,
+            n_estimators=self.n_est,
+        )
+        # Using X_train as target for self-supervised learning.
+        self.xgb_model.fit(X_train, X_train)
+
+    def fit_nn(self, enc_X_train, y_train, X_val=None, y_val=None):
         if X_val is not None:
-            X_val = self.TDE_encoder.transform(X_val)
-
-        if self.debug:
-            print("Shape of the encoded data", enc_X_train.shape)
-        self.nn_model = pytorch_train_ann(enc_X_train, y_train, X_val, y_val, self.shape,
-                                          self.nn, num_of_outputs,
-                                          self.drop, self.n_layers, self.task, self.batchsize, self.checkpoint_name)
-        if self.debug:
-            print('Training is done')
-            
-    def predict(self, X: np.ndarray):
-        self.nn_model.load_state_dict(torch.load(self.checkpoint_name))
-
-        enc_X_test = self.TDE_encoder.transform(X)
-        if self.debug:
-            print(enc_X_test.shape)
-
-        if self.task == 'class':
-            y_hat = pytorch_predict(self.nn_model, enc_X_test, self.task)
-            y_hat = np.rint(y_hat)
-            return y_hat
+            enc_X_val = self.TDE_encoder.transform(X_val)
         else:
-            if self.debug:
-                print('here')
-            y_hat = pytorch_predict(self.nn_model, enc_X_test, self.task)
-            return y_hat
+            enc_X_train, enc_X_val, y_train, y_val = train_test_split(
+                enc_X_train, y_train, test_size=0.2, random_state=42
+            )
 
-    
-    def predict_proba(self, X: np.ndarray):
-        """
-        Only for classification problems. Return probability.
-        """
+        train_loader = DataLoader(
+            dataset=myDataset(enc_X_train, y_train),
+            batch_size=self.batch_size,
+            shuffle=True,
+        )
+        val_loader = DataLoader(
+            dataset=myDataset(enc_X_val, y_val),
+            batch_size=self.batch_size,
+            shuffle=False,
+        )
+        criterion = nn.CrossEntropyLoss() if self.task == "class" else nn.MSELoss()
+        num_of_outputs = len(set(y_train)) if self.task == "class" else 1
+        self.nn_model = NeuralNet(
+            self.input_shape, self.hidden_dim, self.n_layers, num_of_outputs, self.drop
+        ).to(self.device)
+        optimizer = torch.optim.AdamW(self.nn_model.parameters(), lr=1e-3)
+        early_stopping = EarlyStopping(
+            patience=20, verbose=self.debug, path=self.checkpoint_name
+        )
+
+        for epoch in tqdm(range(self.n_epoch), desc="Epochs"):
+            self.nn_model.train()
+            for batch_X, batch_y in train_loader:
+                if self.task == "class":
+                    batch_X, batch_y = batch_X.float().to(
+                        self.device
+                    ), batch_y.long().to(self.device)
+                else:
+                    batch_X, batch_y = batch_X.float().to(
+                        self.device
+                    ), batch_y.float().to(self.device)
+                outputs = self.nn_model(batch_X)
+                loss = criterion(outputs, batch_y)
+                optimizer.zero_grad()
+                loss.backward()
+                optimizer.step()
+
+            # if X_val is not None:
+            self.nn_model.eval()
+            val_loss = 0.0
+            with torch.no_grad():
+                for batch_X, batch_y in val_loader:
+                    if self.task == "class":
+                        batch_X, batch_y = batch_X.float().to(
+                            self.device
+                        ), batch_y.long().to(self.device)
+                    else:
+                        batch_X, batch_y = batch_X.float().to(
+                            self.device
+                        ), batch_y.float().to(self.device)
+                    outputs = self.nn_model(batch_X)
+                    loss = criterion(outputs, batch_y)
+                    val_loss += loss.item()
+
+                val_loss /= len(val_loader)  # Average validation loss
+                early_stopping(val_loss, self.nn_model)
+
+                if early_stopping.early_stop:
+                    print("Early stopping")
+                    break  # Break out of the epoch loop
+
+    def predict(self, X, batch_size=32):
         self.nn_model.load_state_dict(torch.load(self.checkpoint_name))
-        enc_X_test = self.TDE_encoder.transform(X)
+        n_samples = len(X)
+        n_batches = (
+            n_samples + self.batch_size - 1
+        ) // self.batch_size  # Ceiling division
+        y_hats = []
 
-        y_hat = pytorch_predict(self.nn_model, enc_X_test, self.task)
-        return y_hat
+        for i in range(n_batches):
+            start_idx = i * self.batch_size
+            end_idx = min((i + 1) * self.batch_size, n_samples)
 
-def pytorch_predict(model, X, task='class'):
-    model.eval()
-    model.cuda()
-    
-    # TODO ADD BATCH SIZE 
-    
-    mydataset = myDataset(np.array(X))
-    
-    test_loader = torch.utils.data.DataLoader(dataset=mydataset,
-                                               batch_size=1000,
-                                               shuffle=False)
-    out = []
-    for samples in test_loader:
-        samples = samples.cuda().float()
-        if task == 'class':
-            y_hat = torch.softmax(model(samples),-1)[:,1]
-        else:
-            y_hat = model(samples)
-        out.append(y_hat.detach().cpu().numpy().flatten())
-    return [item for sublist in out for item in sublist]
+            batch_X = X[start_idx:end_idx]
+            enc_X_batch = self.TDE_encoder.transform(batch_X)
 
+            if self.task == "class":
+                y_hat = self.nn_model(torch.Tensor(enc_X_batch).to(self.device))
+                y_hat = torch.argmax(y_hat, dim=1).cpu().numpy()
+            else:
+                y_hat = self.nn_model(torch.Tensor(enc_X_batch).to(self.device))
+                y_hat = y_hat.detach().cpu().numpy()
 
+            y_hats.append(y_hat)
 
-
-
+        return np.concatenate(y_hats)
 
 
 class myDataset(Dataset):
-    '''
-    Dataset Class for PyTorch model
-    '''
-
-    def __init__(self, X, y=[]):
-        self.X = X
-        self.y = y
-
-    def __getitem__(self, index):
-        if len(self.y) != 0:
-            return self.X[index], self.y[index]
-        else:
-            return self.X[index]
+    def __init__(self, data, labels):
+        self.data = data
+        self.labels = labels
 
     def __len__(self):
-        return len(self.X)
+        return len(self.data)
 
-
-def swish(x):
-    return x * torch.sigmoid(x)
-
-
-class NeuralNet(nn.Module):
-    def __init__(self, input_size, hidden_dim, n_layers, num_classes, drop=0.25):
-        super(NeuralNet, self).__init__()
-        self.activation = swish
-        self.fc1 = nn.Linear(input_size, hidden_dim)
-        self.layers = nn.ModuleList()
-        self.fc4 = nn.Linear(hidden_dim, num_classes)
-
-        self.dropout1 = nn.Dropout(drop)
-
-        # Hidden Layers (number specified by n_layers)
-        self.layers.extend([nn.Linear(hidden_dim, hidden_dim) for _ in range(n_layers - 1)])
-
-    def forward(self, x):
-
-        x = self.activation(self.fc1(x))
-
-        for layer in self.layers:
-            x = self.dropout1(self.activation(layer(x)))
-
-        x = self.fc4(x)
-
-        return x
-
-
-def create_data_loader(X, y, batch_size, shuffle):
-    dataset = myDataset(X, y)
-    data_loader = DataLoader(dataset=dataset, batch_size=batch_size, shuffle=shuffle)
-    return data_loader
-
-
-def train(model, device, train_loader, criterion, optimizer, task):
-    model.train()
-    for i, (sample, labels) in enumerate(train_loader):
-        # Forward pass
-        outputs = model(sample.float().to(device))
-        if task == 'class':
-            loss = criterion(outputs, labels.long().to(device))
-        else:
-            loss = criterion(outputs.reshape(-1), labels.float().to(device).reshape(-1))
-
-        # Backward and optimize
-        optimizer.zero_grad()
-        loss.backward()
-        optimizer.step()
-        
-
-
-def evaluate(model, device, test_loader, criterion, task='class'):
-    model.eval()
-    test_loss = 0
-    correct = 0
-
-    all_outputs, all_targets = [], []
-
-    with torch.no_grad():
-        for data, target in test_loader:
-            data, target = data.to(device).float(), target.to(device).long()
-            output = model(data)
-            if task == 'class':
-                test_loss += criterion(output, target).item()  # sum up batch loss
-                all_outputs.append(output[:, 1].detach().cpu().numpy())
-            else:
-                test_loss += criterion(output.reshape(-1), target.float().reshape(-1)).item()  # sum up batch loss
-                all_outputs.append(output.detach().cpu().numpy())
-
-            all_targets.append(target.detach().cpu().numpy())
-
-    test_loss /= len(test_loader.dataset)
-    test_loss *= 100
-
-    all_outputs = np.concatenate(all_outputs)
-    all_targets = np.concatenate(all_targets)
-
-    if task == 'class':
-        custom_metric = roc_auc_score(all_targets, all_outputs)
-    else:
-        custom_metric = r2_score(all_targets, all_outputs)
-
-    return test_loss, custom_metric
-
-
-
-def pytorch_train_ann(X_train, y_train, X_val=None, y_val=None, input_size=0, hs1=128, num_outs=1, drop=0.25, n_layers=4,
-                      task='class', batchsize=1024, path_name='checkpoint.pt'):
-    if task == 'class':
-        criterion = nn.CrossEntropyLoss()
-    else:
-        criterion = nn.MSELoss()
-
-    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-
-    if X_val is None or y_val is None:
-        X_train, X_val, y_train, y_val = train_test_split(X_train, y_train, test_size=0.05, stratify=y_train)
-
-    train_loader = create_data_loader(X_train, y_train, batch_size=batchsize, shuffle=True)
-    val_loader = create_data_loader(X_val, y_val, batch_size=batchsize, shuffle=False)
-
-    model = NeuralNet(input_size, hs1, n_layers, num_outs, drop).to(device)
-
-    optimizer = torch.optim.Adam(model.parameters(), lr=1e-3, weight_decay=1e-4)
-
-    patience = 50
-    early_stopping = EarlyStopping(patience=patience, verbose=False, path=path_name)
-
-    avg_train_losses = []
-    train_losses = []
-
-    num_epochs = 10000
-
-    for epoch in tqdm(range(num_epochs), desc="Epochs: "):
-        train(model, device, train_loader, criterion, optimizer, task)
-
-        train_loss, _ = evaluate(model, device, train_loader, criterion, task)
-        val_loss, custom_metric = evaluate(model, device, val_loader, criterion, task)
-
-        early_stopping(val_loss, model)
-        if early_stopping.early_stop:
-            print("Early stopping")
-            print('LOSS:', val_loss)
-            break
-
-    print('Num epochs:', epoch)
-
-    return model
-
-
+    def __getitem__(self, idx):
+        return self.data[idx], self.labels[idx]
 
 
 class EarlyStopping:
     """Early stops the training if validation loss doesn't improve after a given patience."""
+
     # https://github.com/Bjarten/early-stopping-pytorch/blob/master/pytorchtools.py
 
-    def __init__(self, patience=7, verbose=False, delta=0, path='checkpoint.pt', trace_func=print):
+    def __init__(
+        self,
+        patience=7,
+        verbose=False,
+        delta=0.001,
+        path="checkpoint.pt",
+        trace_func=print,
+    ):
         """
         Args:
             patience (int): How long to wait after last time validation loss improved.
                             Default: 7
-            verbose (bool): If True, prints a message for each validation loss improvement. 
+            verbose (bool): If True, prints a message for each validation loss improvement.
                             Default: False
             delta (float): Minimum change in the monitored quantity to qualify as an improvement.
                             Default: 0
             path (str): Path for the checkpoint to be saved to.
                             Default: 'checkpoint.pt'
             trace_func (function): trace print function.
-                            Default: print            
+                            Default: print
         """
         self.patience = patience
         self.verbose = verbose
@@ -309,8 +250,8 @@ class EarlyStopping:
         self.delta = delta
         self.path = path
         self.trace_func = trace_func
-    def __call__(self, val_loss, model):
 
+    def __call__(self, val_loss, model):
         score = -val_loss
 
         if self.best_score is None:
@@ -318,7 +259,7 @@ class EarlyStopping:
             self.save_checkpoint(val_loss, model)
         elif score < self.best_score + self.delta:
             self.counter += 1
-            #self.trace_func(f'EarlyStopping counter: {self.counter} out of {self.patience}')
+            # self.trace_func(f'EarlyStopping counter: {self.counter} out of {self.patience}')
             if self.counter >= self.patience:
                 self.early_stop = True
         else:
@@ -327,8 +268,34 @@ class EarlyStopping:
             self.counter = 0
 
     def save_checkpoint(self, val_loss, model):
-        '''Saves model when validation loss decrease.'''
+        """Saves model when validation loss decrease."""
         if self.verbose:
-            self.trace_func(f'Validation loss decreased ({self.val_loss_min:.6f} --> {val_loss:.6f}).  Saving model ...')
+            self.trace_func(
+                f"Validation loss decreased ({self.val_loss_min:.6f} --> {val_loss:.6f}).  Saving model ..."
+            )
         torch.save(model.state_dict(), self.path)
         self.val_loss_min = val_loss
+
+
+class NeuralNet(nn.Module):
+    def __init__(self, input_dim, hidden_dim, n_layers, num_classes, drop):
+        super(NeuralNet, self).__init__()
+
+        layers = []
+        layers.append(nn.Linear(input_dim, hidden_dim))
+        layers.append(nn.BatchNorm1d(hidden_dim))
+        layers.append(nn.PReLU())
+        layers.append(nn.Dropout(drop))
+
+        for _ in range(n_layers):
+            layers.append(nn.Linear(hidden_dim, hidden_dim))
+            layers.append(nn.BatchNorm1d(hidden_dim))
+            layers.append(nn.ReLU())
+            layers.append(nn.Dropout(drop))
+
+        layers.append(nn.Linear(hidden_dim, num_classes))
+
+        self.model = nn.Sequential(*layers)
+
+    def forward(self, x):
+        return self.model(x)
--- src/tde.py
+++ src/tde.py
@@ -1,61 +1,59 @@
 import re
-import gc
 import numpy as np
-import multiprocessing
-from multiprocessing import Pool
+from typing import List
+from collections import defaultdict, Counter
+
+node_info_re = re.compile(r"f(\d+)<([0-9\.]+)")
+
+
+def parse_node_info(node_info: str):
+    matched = node_info_re.findall(node_info)
+    if not matched:
+        return None, None
+    feature, threshold = matched[0]
+    return int(feature), float(threshold)
+
+
+def extract_node_data_from_tree(tree_number, tree):
+    node_data = defaultdict(dict)
+    for node in tree.split("\n"):
+        cleaned_node = re.sub(r"\s+", "", node)
+        if ":" not in cleaned_node:
+            continue
+        node_id, node_info = map(str.strip, cleaned_node.split(":"))
+        feature, threshold = parse_node_info(node_info)
+        if feature is not None and threshold is not None:
+            node_data[(tree_number, int(node_id))] = {
+                "feature": feature,
+                "threshold": threshold,
+            }
+    return node_data
 
 
 class TreeDrivenEncoder:
-    """
-    TreeDrivenEncoder algorithm.
-
-    OOP is beautiful
-    """
-
-    def __init__(self):
-        self.all_conditions = None
-
-    def fit(self, trees):
+    def __init__(self, min_freq=2):
         self.all_conditions = []
-        node_data = {}
+        self.min_freq = min_freq
 
+    def fit(self, trees: List[str]):
+        node_data = defaultdict(dict)
         for tree_number, tree in enumerate(trees):
-            tree_nodes = tree.split('\n\t')
-            for node in tree_nodes:
-                cleaned_node = re.sub(r'\s+', '', node)
-                node_id = int(cleaned_node.split(':')[0])
-                node_info = cleaned_node.split(':')[1]
+            node_data.update(extract_node_data_from_tree(tree_number, tree))
 
-                if 'leaf' not in node_info:
-                    key = (tree_number, node_id)
-                    node_data[key] = self.get_node_data(node_info)
+        feature_counter = Counter(data["feature"] for data in node_data.values())
 
-        for key in sorted(node_data.keys()):
-            feature, value, _ = node_data[key]
-            if value != 0:
-                self.all_conditions.append([feature, value])
+        self.all_conditions = [
+            data
+            for key, data in sorted(node_data.items())
+            if feature_counter[data["feature"]] >= self.min_freq
+        ]
 
-    def transform(self, data):
-        data_array = np.array(data)
-        with Pool(processes=max(1, multiprocessing.cpu_count() - 2)) as pool:
-            encoded_data = pool.map(self._dt2v, data_array)
-        
-        gc.collect()
-        return np.array(encoded_data)
-
-    def _dt2v(self, sample, debug=False):
-        node_vector = []
-        for feature, value in self.all_conditions:
-            if debug: print(feature, value)
-            node_vector.append(int(sample[feature] < float(value)))
-
-        if debug: print(node_vector)
-        return node_vector
-
-    @staticmethod
-    def get_node_data(node_info):
-        condition = re.findall(r'\[([^]]*)\]', node_info)
-        feature, value = re.split(r'[`=<>]', condition[0])
-        feature = int(feature[1:])
-        value = float(value)
-        return [feature, value, 0]
+    def transform(self, X):
+        X = np.array(X)
+        n_conditions = len(self.all_conditions)
+        encoded_X = np.zeros((X.shape[0], n_conditions), dtype=int)
+        for i, condition in enumerate(self.all_conditions):
+            encoded_X[:, i] = (
+                X[:, condition["feature"]] < condition["threshold"]
+            ).astype(int)
+        return encoded_X
